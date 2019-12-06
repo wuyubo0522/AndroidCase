8e696a7142570d546cf62b0756827732572ee896
yubo <wuyubo0522@163.com>
2019-12-06 18:10:40 +0800

    升级项目build.gradle的配置，创建1.0.5的例子的搭建的model

---
+++ recycleviewdemo/src/main/java/com/yb/recycleview_demo/RecycleViewActivity.kt
@@ -0,0 +1,12 @@
+package com.yb.recycleview_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecycleViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycle_view)
+    }
+}
