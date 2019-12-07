f67825a9ab25cc07e1e9646fbcb31d645e1670aa
yubo <wuyubo0522@163.com>
2019-12-07 10:46:29 +0800

    RecycleView组件，多种列表按钮创建完成

---
+++ recycleviewdemo/src/main/java/com/yb/recycleview_demo/SingleRowActivity.kt
@@ -0,0 +1,18 @@
+package com.yb.recycleview_demo
+
+import com.yb.common.base_mvc.BaseActivityC
+
+/**
+ * 类说明：单行列表界面
+ * @author 裕博
+ * Time: 2019/12/7 10:38
+ */
+class SingleRowActivity :BaseActivityC(){
+    override fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- recycleviewdemo/src/main/java/com/yb/recycleview_demo/main/RecycleViewActivity.kt
+++ recycleviewdemo/src/main/java/com/yb/recycleview_demo/main/RecycleViewActivity.kt
@@ -1,16 +1,19 @@
-package com.yb.recycleview_demo
+package com.yb.recycleview_demo.main
 
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.gyf.immersionbar.ImmersionBar
 import com.yb.common.ARouterManager
 import com.yb.common.base_mvc.BaseActivityC
+import com.yb.recycleview_demo.R
 
 /**
- * 类说明：RecycleView的多种组件
+ * 类说明：RecycleView的多种应用组件
  * @author 裕博
  */
 @Route(path = ARouterManager.RECYCLE_VIEW_DEOM)
 class RecycleViewActivity : BaseActivityC() {
     override fun init() {
+        ImmersionBar.with(this).init()
     }
 
     override fun getLayoutId(): Int {
